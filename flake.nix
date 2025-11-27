f4746617a158820f9dd297992c5b83d7c538e1cf
Macs1324 <max.blank410@gmail.com>
2025-11-27 20:12:18 +0100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Toy payment engine - Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
